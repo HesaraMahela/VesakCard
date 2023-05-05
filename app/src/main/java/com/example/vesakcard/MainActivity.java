93723bbd5cf546e8de887ebdfb1e6effb2a07827
HesaraMahela <hesaramahela@outlook.com>
2023-05-05 09:15:05 +0530

    adding greeting page and movement between actions

--- app/src/main/java/com/example/vesakcard/MainActivity.java
+++ app/src/main/java/com/example/vesakcard/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.vesakcard;
 
+import android.content.Intent;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+            case (MotionEvent.ACTION_MOVE) :
+                startActivity(new Intent(MainActivity.this, greeting_page.class));
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
